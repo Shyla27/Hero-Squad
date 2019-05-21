9cae9f8be1e114d5070220ff07d73e2494c517cd
SHYLA <sheilabernicew@gmail.com>
2019-05-21 09:12:31 +0300

    added content that ensures unnecessary build files aren't pushed to heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -51,17 +51,6 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // post("/heroes", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   String name = request.queryParams("name");
-    //   int age = Integer.parseInt(request.queryParams("age"));
-    //   String power = request.queryParams("power");
-    //   String weakness = request.queryParams("weakness");
-    //   Hero newHero = new Hero(name, age, power, weakness);
-    //
-    //   model.put("template", "templates/success.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
 
     get("squads/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
